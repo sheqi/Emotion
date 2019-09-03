be9f6a150ccdcc35059c778f3cb565b330c63118
sheqi <sheqi1991@gmail.com>
2019-09-03 21:21:32 +0800

    Emotion

---
+++ adaptiveEmo/constants_3.py
@@ -0,0 +1,8 @@
+CASC_PATH = './haarcascade_files/haarcascade_frontalface_default.xml'
+SIZE_FACE = 48
+EMOTIONS = ['negative', 'neutral', 'positive']
+SAVE_DIRECTORY = './data/'
+SAVE_MODEL_FILENAME = 'Gudi_model_100_epochs_20000_faces_3'
+DATASET_CSV_FILENAME = 'fer2013_3.csv'
+SAVE_DATASET_IMAGES_FILENAME = 'data_images_3.npy'
+SAVE_DATASET_LABELS_FILENAME = 'data_labels_3.npy'
